ceee2cb31cadde0bb6b8c719679a2c625d9be751
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-06 15:47:10 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,13 +1,13 @@
-class Patient 
+class Patient
 
-  attr_accessor :name 
+  attr_accessor :name
   @@all = []
 
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
+    @name = name
+    @@all << self
+  end
   
   def self.all
     @@all
-  end 
+  end
